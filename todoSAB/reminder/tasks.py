a3a4e73ca357c53daf10851eb4ef3764734a262c
igor <igor@igorlenovog700.com>
2017-10-20 16:24:25 +0300

    setting of task of celery sending email

--- todoSAB/reminder/tasks.py
+++ todoSAB/reminder/tasks.py
@@ -1,8 +1,30 @@
 from __future__ import unicode_literals
 
-from celery import task, shared_task
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+from reminder.models import Reminder
+from django.core.exceptions import ObjectDoesNotExist
 
 @shared_task
 #@task
-def exceuteTask(arg):
-    print("1111111111111111111111")
+def exceuteTask(id):
+    
+    try:
+        entry = Reminder.objects.get(pk=1)
+        print("working with", entry.name, entry.email)
+        
+        #try:
+        #    subject = 'Reminder'
+        #    message = entry.name
+        #    to_list = [entry.email]
+        #    send_mail(subject, message, settings.EMAIL_HOST_USER, to_list, fail_silently=True)
+        #finally:
+        #    print("Unable to send an email", entry.name, entry.email)
+        
+    except ObjectDoesNotExist:
+        pass
+
+    
+    
+    
--- todoSAB/reminder/views.py
+++ todoSAB/reminder/views.py
@@ -7,6 +7,23 @@ from rest_framework import viewsets
 from reminder.models import Reminder
 from .serializers import ReminderSerializer
 
+from rest_framework import status
+from rest_framework.response import Response
+from datetime import datetime, timedelta
+from .tasks import exceuteTask
+
 class ReminderViewSet(viewsets.ModelViewSet):
     queryset = Reminder.objects.all() 
     serializer_class = ReminderSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        
+        time_run = datetime.utcnow() + timedelta(minutes=int(serializer.data['delay']))
+        exceuteTask.apply_async((serializer.data['id'],), eta=time_run)
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
